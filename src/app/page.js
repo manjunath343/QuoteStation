18a4c34e14447af142695878d52af9bd33dbe258
manjunath343 <manjunathmajestic343@gmail.com>
2025-03-21 10:41:49 +0530

    "changed-1"

--- src/app/page.js
+++ src/app/page.js
@@ -136,10 +136,10 @@ export default function HomePage() {
         <h1 className="text-4xl font-bold text-yellow-300">QuoteStation</h1>
         <div className="flex gap-4">
           <button onClick={() => setShowWritePopup(true)} className="flex items-center gap-2 px-4 py-2 bg-green-600 hover:bg-green-700 text-white rounded-md shadow-lg">
-            <FaPen /> Write
+            <FaPen /> 
           </button>
           <button className="flex items-center gap-2 px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-md shadow-lg" onClick={()=> router.push("/admin-login")}>
-            <FaUserShield /> Admin
+            <FaUserShield /> 
           </button>
         </div>
       </header>
